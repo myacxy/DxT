ca4fa927875beb204aa401638fe1ed5a077ef135
myacxy <myacxy@gmail.com>
2014-10-17 16:33:32 +0200

    CharLimiterDialog comments

--- app/src/main/java/net/myacxy/dashclock/twitch/ui/CharLimiterDialog.java
+++ app/src/main/java/net/myacxy/dashclock/twitch/ui/CharLimiterDialog.java
@@ -66,25 +66,28 @@ public class CharLimiterDialog extends Preference {
                 R.layout.dialog_char_limiter, null);
         builder.setView(layout);
 
+        final TextView preview = (TextView) layout.findViewById(R.id.pref_char_limiter_preview);
+        final NumberPicker numberPicker = (NumberPicker) layout.findViewById(R.id.pref_char_limiter_value);// setup number picker
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(200);
         // retrieve previous setting
         final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        final int previousLimit = sp.getInt(TwitchExtension.PREF_CHAR_LIMIT, 200);
+        numberPicker.setValue(previousLimit);
+
         final String longestBody = sp.getString(TwitchExtension.PREF_LONGEST_BODY,
                 getContext().getResources().getString(R.string.lorem_ipsum));
+
+        // gray out the text that is beyond the given character limit
         SpannableString text = new SpannableString(longestBody);
         final int color = getContext().getResources().getColor(android.R.color.darker_gray);
         final int color2 = getContext().getResources().getColor(android.R.color.black);
-        final int previousValue = sp.getInt(TwitchExtension.PREF_CHAR_LIMIT, 200);
-        final NumberPicker numberPicker = (NumberPicker) layout.findViewById(R.id.pref_char_limiter_value);
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(200);
-        numberPicker.setValue(previousValue);
-
         final int maxLength = longestBody.length();
-        text.setSpan(new ForegroundColorSpan(color), previousValue > maxLength ? maxLength : previousValue, maxLength, 0);
+        text.setSpan(new ForegroundColorSpan(color),
+                previousLimit > maxLength ? maxLength : previousLimit, maxLength, 0);
+        preview.setText(text);
 
-        final TextView textView = (TextView) layout.findViewById(R.id.pref_char_limiter_text);
-        textView.setText(text);
-
+        // dynamically change preview on changed value
         numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
@@ -93,7 +96,7 @@ public class CharLimiterDialog extends Preference {
                         newVal  > maxLength ? maxLength : newVal, 0);
                 newText.setSpan(new ForegroundColorSpan(color),
                         newVal > maxLength ? maxLength : newVal, maxLength, 0);
-                textView.setText(newText);
+                preview.setText(newText);
             }
         });
 
@@ -101,18 +104,17 @@ public class CharLimiterDialog extends Preference {
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
                 // notify change listener
                 getOnPreferenceChangeListener().onPreferenceChange(CharLimiterDialog.this,
                         numberPicker.getValue());
             }
         });
 
-        // reset to previous value on ok
+        // reset to previous value on cancel
         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                sp.edit().putInt(TwitchExtension.PREF_CHAR_LIMIT, previousValue).apply();
+                sp.edit().putInt(TwitchExtension.PREF_CHAR_LIMIT, previousLimit).apply();
             }
         });
         builder.create();
